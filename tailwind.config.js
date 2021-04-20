18f190f360bbd0b58033a3db37f2b9a7e4a5ed90
Nick Holmstedt <holmstn@gmail.com>
2021-04-20 18:00:20 -0400

    font updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+    },
     colors: {
+      white: colors.white,
+      red: colors.red,
       gray: colors.blueGray,
       indigo: colors.indigo,
       cyan: { ...colors.cyan, 500: "#89e4fd" },
